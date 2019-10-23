a7f019490e996853e71d613c901b57a6865287d3
IceHole123 <55634278+IceHole123@users.noreply.github.com>
2019-10-23 13:51:08 +0200

    Add files via upload

---
+++ srcipt.js
@@ -0,0 +1,203 @@
+function menunavsortie() {
+    var element = document.getElementById("mnav");
+    element.classList.remove("navrentre");
+    var element = document.getElementById("mnav");
+    element.classList.add("navsortie");
+  }
+
+function menunavrentre() {
+    var element = document.getElementById("mnav");
+    element.classList.remove("navsortie");
+    var element = document.getElementById("mnav");
+    element.classList.add("navrentre");
+}
+function neptuneon() {
+  var element = document.getElementById("nept");
+  element.classList.remove("neptuneout");
+  var element = document.getElementById("nept");
+  element.classList.add("neptuneon");
+  var element = document.getElementById("imgnept");
+  element.classList.remove("imgneptout");
+  var element = document.getElementById("imgnept");
+  element.classList.add("imgnepton");
+}
+
+function neptuneout() {
+  var element = document.getElementById("nept");
+  element.classList.remove("neptuneon");
+  var element = document.getElementById("nept");
+  element.classList.add("neptuneout");
+  var element = document.getElementById("imgnept");
+  element.classList.remove("imgnepton");
+  var element = document.getElementById("imgnept");
+  element.classList.add("imgneptout");
+}
+function uranuson() {
+  var element = document.getElementById("uran");
+  element.classList.remove("uranusout");
+  var element = document.getElementById("uran");
+  element.classList.add("uranuson");
+  var element = document.getElementById("imguran");
+  element.classList.remove("imguranout");
+  var element = document.getElementById("imguran");
+  element.classList.add("imguranon");
+}
+
+function uranusout() {
+  var element = document.getElementById("uran");
+  element.classList.remove("uranuson");
+  var element = document.getElementById("uran");
+  element.classList.add("uranusout");
+  var element = document.getElementById("imguran");
+  element.classList.remove("imguranon");
+  var element = document.getElementById("imguran");
+  element.classList.add("imguranout");
+}
+function saturneon() {
+  var element = document.getElementById("satu");
+  element.classList.remove("saturneout");
+  var element = document.getElementById("satu");
+  element.classList.add("saturneon");
+  var element = document.getElementById("imgsatu");
+  element.classList.remove("imgsatuout");
+  var element = document.getElementById("imgsatu");
+  element.classList.add("imgsatuon");
+}
+
+function saturneout() {
+  var element = document.getElementById("satu");
+  element.classList.remove("saturneon");
+  var element = document.getElementById("satu");
+  element.classList.add("saturneout");
+  var element = document.getElementById("imgsatu");
+  element.classList.remove("imgsatuon");
+  var element = document.getElementById("imgsatu");
+  element.classList.add("imgsatuout");
+}
+function jupiteron() {
+  var element = document.getElementById("jupi");
+  element.classList.remove("jupiterout");
+  var element = document.getElementById("jupi");
+  element.classList.add("jupiteron");
+  var element = document.getElementById("imgjupi");
+  element.classList.remove("imgjupiout");
+  var element = document.getElementById("imgjupi");
+  element.classList.add("imgjupion");
+}
+
+function jupiterout() {
+  var element = document.getElementById("jupi");
+  element.classList.remove("jupiteron");
+  var element = document.getElementById("jupi");
+  element.classList.add("jupiterout");
+  var element = document.getElementById("imgjupi");
+  element.classList.remove("imgjupion");
+  var element = document.getElementById("imgjupi");
+  element.classList.add("imgjupiout");
+}
+function marson() {
+  var element = document.getElementById("mars");
+  element.classList.remove("marsout");
+  var element = document.getElementById("mars");
+  element.classList.add("marson");
+  var element = document.getElementById("imgmars");
+  element.classList.remove("imgmarsout");
+  var element = document.getElementById("imgmars");
+  element.classList.add("imgmarson");
+}
+
+function marsout() {
+  var element = document.getElementById("mars");
+  element.classList.remove("marson");
+  var element = document.getElementById("mars");
+  element.classList.add("marsout");
+  var element = document.getElementById("imgmars");
+  element.classList.remove("imgmarson");
+  var element = document.getElementById("imgmars");
+  element.classList.add("imgmarsout");
+}
+function terreon() {
+  var element = document.getElementById("terr");
+  element.classList.remove("terreout");
+  var element = document.getElementById("terr");
+  element.classList.add("terreon");
+  var element = document.getElementById("imgterr");
+  element.classList.remove("imgterrout");
+  var element = document.getElementById("imgterr");
+  element.classList.add("imgterron");
+}
+
+function terreout() {
+  var element = document.getElementById("terr");
+  element.classList.remove("terreon");
+  var element = document.getElementById("terr");
+  element.classList.add("terreout");
+  var element = document.getElementById("imgterr");
+  element.classList.remove("imgterron");
+  var element = document.getElementById("imgterr");
+  element.classList.add("imgterrout");
+}
+function venuson() {
+  var element = document.getElementById("venu");
+  element.classList.remove("venusout");
+  var element = document.getElementById("venu");
+  element.classList.add("venuson");
+  var element = document.getElementById("imgvenu");
+  element.classList.remove("imgvenuout");
+  var element = document.getElementById("imgvenu");
+  element.classList.add("imgvenuon");
+}
+
+function venusout() {
+  var element = document.getElementById("venu");
+  element.classList.remove("venuson");
+  var element = document.getElementById("venu");
+  element.classList.add("venusout");
+  var element = document.getElementById("imgvenu");
+  element.classList.remove("imgvenuon");
+  var element = document.getElementById("imgvenu");
+  element.classList.add("imgvenuout");
+}
+function mercureon() {
+  var element = document.getElementById("merc");
+  element.classList.remove("mercureout");
+  var element = document.getElementById("merc");
+  element.classList.add("mercureon");
+  var element = document.getElementById("imgmerc");
+  element.classList.remove("imgmercout");
+  var element = document.getElementById("imgmerc");
+  element.classList.add("imgmercon");
+}
+
+function mercureout() {
+  var element = document.getElementById("merc");
+  element.classList.remove("mercureon");
+  var element = document.getElementById("merc");
+  element.classList.add("mercureout");
+  var element = document.getElementById("imgmerc");
+  element.classList.remove("imgmercon");
+  var element = document.getElementById("imgmerc");
+  element.classList.add("imgmercout");
+}
+function soleilon() {
+  var element = document.getElementById("sole");
+  element.classList.remove("soleilout");
+  var element = document.getElementById("sole");
+  element.classList.add("soleilon");
+  var element = document.getElementById("imgsole");
+  element.classList.remove("imgsoleout");
+  var element = document.getElementById("imgsole");
+  element.classList.add("imgsoleon");
+}
+
+function soleilout() {
+  var element = document.getElementById("sole");
+  element.classList.remove("soleilon");
+  var element = document.getElementById("sole");
+  element.classList.add("soleilout");
+  var element = document.getElementById("imgsole");
+  element.classList.remove("imgsoleon");
+  var element = document.getElementById("imgsole");
+  element.classList.add("imgsoleout");
+}
+
